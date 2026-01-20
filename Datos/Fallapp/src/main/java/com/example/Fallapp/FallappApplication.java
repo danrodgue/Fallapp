e61f92f73f975898c92c0e052b3b721f53dfa57d
danrodgue <danrodgue@users.noreply.github.com>
2026-01-20 10:36:26 +0100

    AÃ±adir Spring inicial al repo Fallapp

---
+++ Datos/Fallapp/src/main/java/com/example/Fallapp/FallappApplication.java
@@ -0,0 +1,13 @@
+package com.example.Fallapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FallappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FallappApplication.class, args);
+	}
+
+}
---
+++ Datos/Fallapp/src/test/java/com/example/Fallapp/FallappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Fallapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FallappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
