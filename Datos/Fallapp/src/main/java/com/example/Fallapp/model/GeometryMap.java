3c5e779e392dbed6a4289b53f7b5b4786ffbf2a2
Jose Burgos <joseburgosmtnz@gmail.com>
2026-01-26 11:51:10 +0100

    Endpoint que da acceso a las coordenadas hecho

---
+++ Datos/Fallapp/src/main/java/com/example/Fallapp/model/GeometryMap.java
@@ -0,0 +1,15 @@
+package com.example.Fallapp.model;
+
+import java.util.List;
+
+public class Geometry {
+    private List<Double> coordinates;
+
+    public List<Double> getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(List<Double> coordinates) {
+        this.coordinates = coordinates;
+    }
+}
